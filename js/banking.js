426a95d6aeff1a7d3fa84fe657f7337bab7b617d
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2022-02-14 00:58:27 +0600

    condition added deposite

--- js/banking.js
+++ js/banking.js
@@ -1,3 +1,4 @@
+// get input value;
 function getInputValue(inputId){
     const InputField = document.getElementById(inputId);
     const inputAmount = InputField.value;
@@ -6,40 +7,55 @@ function getInputValue(inputId){
     InputField.value = '';
     return inputAmountConvert;
 }
-
-
-document.getElementById("deposite-btn").addEventListener("click",function(){
-    const depositAmountConvert = getInputValue('deposite-amount');
-
-    const depositTotal = document.getElementById("deposite-total");
-    const depositTotalText = depositTotal.innerText;
-    const depositTotalConvert = parseFloat(depositTotalText);
-
-    const totalDepositAmount = depositAmountConvert + depositTotalConvert;
-    depositTotal.innerText = totalDepositAmount;
-    // total balance added;
+// update total field
+function updateTotalField(totalAmountConvert,totalFieldId){
+    const totalElement = document.getElementById(totalFieldId);
+    const TotalText = totalElement.innerText;
+    const TotalConvert = parseFloat(TotalText);
+    const totalDepositAmount = totalAmountConvert + TotalConvert;
+    totalElement.innerText = totalDepositAmount;
+}
+// update balance
+function updateBalance(depositAmountConvert,isAdd){
+    // debugger;
+    const balanceTotal = document.getElementById("balance-total");
+    const previousBalanceTotal = getCurrentBalance();
+    if(isAdd == true){
+        const totalBalance = previousBalanceTotal + depositAmountConvert;
+        balanceTotal.innerText = totalBalance;
+    }
+    else{
+        const totalBalance = previousBalanceTotal - depositAmountConvert;
+        balanceTotal.innerText = totalBalance;
+    }
+}
+// get current balance;
+function getCurrentBalance(){
     const balanceTotal = document.getElementById("balance-total");
     const balanceTotalText = balanceTotal.innerText;
     const previousBalanceTotal = parseFloat(balanceTotalText);
-
-    const totalBalance = previousBalanceTotal + depositAmountConvert;
-    balanceTotal.innerText = totalBalance;
-    
+    return previousBalanceTotal;
+}
+document.getElementById("deposite-btn").addEventListener("click",function(){
+    const depositAmountConvert = getInputValue('deposite-amount');
+    // update deposite amount
+    if(depositAmountConvert > 0){
+        updateTotalField(depositAmountConvert,"deposite-total");
+        updateBalance(depositAmountConvert,true);
+    }
+    else{
+        alert("your value is confused")
+    }
 });
 // handle withdraw buttton;
 document.getElementById("withdraw-btn").addEventListener("click",function(){
     const withdrawAmountCount = getInputValue("withdraw-conut")
-
-    const withdrawAmount = document.getElementById("withdraw-amount");
-    const withdrawAmountTotalText = withdrawAmount.innerText;
-    const withdrawAmountTotal = parseFloat(withdrawAmountTotalText);
-
-    const totalwithdrawBalance = withdrawAmountCount + withdrawAmountTotal;
-    withdrawAmount.innerText = totalwithdrawBalance;
-    // current balance
-    const balanceTotal = document.getElementById("balance-total");
-    const balanceTotalText = balanceTotal.innerText;
-    const previousBalanceTotal = parseFloat(balanceTotalText);
-
-    balanceTotal.innerHTML = previousBalanceTotal - withdrawAmountCount;
+    const currentBalance = getCurrentBalance()
+    if(withdrawAmountCount > 0 && withdrawAmountCount <= currentBalance){
+        updateTotalField(withdrawAmountCount,'withdraw-amount')
+        updateBalance(withdrawAmountCount,false)
+    }
+    else{
+        alert("your value is confused")
+    }
 })
